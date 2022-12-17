d15621b13af69e044ac2e490bcf008e96785e7d8
ngima <ngima.sherpa8681@gmail.com>
2022-12-18 00:46:39 +0545

    forget passwoed userlist userById model added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,3 +79,66 @@ exports.login = async (req,res) =>{
     }
 }
 
+/// forgate passwor
+exports.forgetPassword= async(req,res)=>{
+    let user = await User.findOne({email:req.body.email})
+    if(!user) return res.status(400).json({ error: 'sorry the email you provieded not found  please register first or try anaoatrher' })
+// check if user is already verified
+ if(user.isVerified === false) return res.status(400).json({ error: 'this email is not varefied please verify' })
+ 
+
+   token=  generateAuthToken(user)
+    if(!token){
+        return res.status(400).json({ error: 'something went wrong' })
+    }
+
+    const c_url = `${process.env.CLIENT_URL}/resetpassword/${token.token}`
+    //send email
+    sendEmail({
+        from:process.env.SMTP_USER,
+        to:user.email,
+        subject:"reset password  link",
+        text:`hello, \n\n please reste  your password by cliking below link below Link:\n\n http:\/\/${req.headers.host}\/api\/resetpassword\/${token.token}`,
+        html:`<a href='${c_url}'  >Click here to Reset Password </a>`
+    })
+    res.json({message: ' password reset link has been sent'})
+}
+
+/// reset password
+
+// exports.resetPassword = async (req,res)=>{
+//     let token = await Token.findOne({token:req.params.token})
+//          if( !token){
+//              return res.status(400).json({ error: 'invalid token or token may have exprire' })
+//          }
+ 
+     
+//      let user = await user.findOne({_id:token.userId})
+//         if(!user){
+//             return res.status(400).json({ error: 'invalid user for this token ' })
+//          }
+//      user.password= req.body.password
+//      user = await user.save()
+//      if(!user){
+//          return res.status(400).json({ error: 'failed to reset password ' })
+//      }
+//      res.json({message:"password has been rest"})
+//  }
+ 
+// user list
+
+exports.userList = async (req,res)=>{
+    const user = await userModel.find().select('-hashed_password -salt')
+    if(!user)  return res.status(400).json({ error: 'something went wrong ' })
+    res.send(user)
+}
+
+// single user
+
+exports.singleUserView = async(req,res)=>{
+    const {id}= req.params
+    const user = await userModel.findById({_id:id}).select('-hashed_password -salt')
+    if(!user) return res.status(400).json({ error: 'something went wrong ' })
+    res.send(user)
+}
+
