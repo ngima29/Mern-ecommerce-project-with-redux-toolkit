7d24260bef7759bc2c1d01bd8ee564cba89dc1b3
ngima <ngima.sherpa8681@gmail.com>
2022-12-21 00:37:48 +0545

    login and regstration added in react toolkits

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Register from './pages/Register'
 
 const MyRoutes = () => {
@@ -9,7 +10,7 @@ const MyRoutes = () => {
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/register' element={<Register/>}/>
-
+    <Route path='/login' element={<Login/>}/>
   </Routes>
   </BrowserRouter>
   )
--- frontend/src/reducer/authSlice.js
+++ frontend/src/reducer/authSlice.js
@@ -10,7 +10,7 @@ const initialState={
     success: "",
     user:"",
     error:"",
-    loading:false,
+    userLoaded:false,
     status:""
 };
 export const registerUser = createAsyncThunk(
@@ -30,12 +30,58 @@ export const registerUser = createAsyncThunk(
       return rejectWithValue(error.response.data)
     }
 })
+/// user login 
+export const userLogin = createAsyncThunk(
+    "user/login",
+    async(value, {rejectWithValue })=>{
+        try {
+            const token = await axios.post(`${api}/user/login`,{
+                name:value.email,
+                password:value.password,
+            });
+            localStorage.setItem('token',token.data)
+            return token.data
+        } catch (error) {
+            console.log(error.response);
+      return rejectWithValue(error.response.data);
+        }
+    }
+);
 
 const authSlice = createSlice({
 name:'user',
 initialState,
 reducers:{
-
+ loasUser(state,action) {
+    const token = state.token
+    if(token){
+        const user = jwt_decode(action.payload, { header: true });
+        return{
+            ...state,
+            token,
+            name:user.name,
+            email:user.email,
+            _id:user._id,
+            userLoaded:true
+        }
+    }
+ },
+ logoutUser(state, action){
+    localStorage.removeItem('token')
+    return{
+    ...state,
+    token:"",
+     name:'',
+     email:'',
+    _id:"",
+    success: "",
+    user:"",
+    error:"",
+    userLoaded:false,
+    status:""
+    }
+ }
+ // reducer close
 },
 extraReducers:(builder)=>{
 builder.addCase(registerUser.pending,(state,action)=>{
@@ -63,12 +109,38 @@ builder.addCase(registerUser.rejected,(state,action)=>{
     error:action.payload
    }
 });
+builder.addCase(userLogin.pending,(state,action)=>{
+    return{
+        ...state, status:"pending"
+    }
+});
+builder.addCase(userLogin.fulfilled,(state,action)=>{
+    if(action.payload){
+        const user = jwt_decode(action.payload, { header: true });
+       
+         return{
+          ...state,
+          token: action.payload,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          status: "success",
+          
+         } 
+      } else return state
+});
+builder.addCase(userLogin.rejected,(state,action)=>{
+    return{
+        ...state,
+         status:"rejected",
+        error:action.payload
+    }
+});
+// extraReducer close
 }
 
-
-
-
+///create slice ended
 })
 
-//export const { } = authSlice.actions
+export const {loasUser, logoutUser} = authSlice.actions
 export default authSlice.reducer
